c2df17fe95d8766cbeed097ad62722adb33f8cb1
Alexey Nikitin <tank@bohr.su>
2022-12-09 22:17:56 +0400

    Day9. Part 2

--- day9.erl
+++ day9.erl
@@ -1,24 +1,22 @@
 -module(day9).
 
 -export([part1/0]).
+-export([part2/0]).
 
 -record(motion, {
     direction :: right | up | left | down,
     steps     :: non_neg_integer()
 }).
 
--record(state, {
-    head = {0, 0},
-    tail = {0, 0}
-}).
+part1() -> run(2).
+part2() -> run(10).
 
-part1() ->
+run(RopeLength) ->
     {ok, Input} = file:read_file("day9.input"),
     Movements = parse(Input),
-    States = play(Movements),
-    % States.
-    Tails = lists:map(fun(#state{tail = Tail}) -> Tail end, States),
-    % lists:usort(Tails).
+    Rope = init_rope(RopeLength),
+    Ropes = play(Movements, [Rope]),
+    Tails = lists:map(fun lists:last/1, Ropes),
     length(lists:uniq(Tails)).
 
 parse(Input) ->
@@ -44,32 +42,38 @@ direction(<<"L">>) -> left;
 direction(<<"D">>) -> down;
 direction(<<"U">>) -> up.
 
-play(Motions) ->
-    play(Motions, [#state{}]).
+play([], Ropes) ->
+    lists:reverse(Ropes);
+play([Motion | Motions], Ropes) ->
+    play(Motions, run_motion(Motion, Ropes)).
 
-play([], Acc) ->
-    lists:reverse(Acc);
-play([Motion | Motions], Acc) ->
-    play(Motions, run_motion(Motion, Acc)).
-
-run_motion(#motion{steps = 0}, Acc) ->
-    Acc;
-run_motion(#motion{direction = Direction, steps = Steps} = Motion, [State | _] = Acc) ->
-    Head = move_head(Direction, State#state.head),
-    Tail = move_next(Head, State#state.tail),
-    run_motion(Motion#motion{steps = Steps - 1}, [State#state{head = Head, tail = Tail} | Acc]).
+run_motion(#motion{steps = 0}, Ropes) ->
+    Ropes;
+run_motion(#motion{direction = Direction, steps = Steps} = Motion, [[Head | Rest] | _] = Ropes) ->
+    NewRope = lists:reverse(lists:foldl(fun(Knot, [Prev | _] = Acc) ->
+        [move_knot(Prev, Knot) | Acc]
+    end, [move_head(Direction, Head)], Rest)),
+    run_motion(Motion#motion{steps = Steps - 1}, [NewRope | Ropes]).
 
 move_head(right, {X, Y}) -> {X + 1, Y};
 move_head(left, {X, Y}) -> {X - 1, Y};
 move_head(down, {X, Y}) -> {X, Y - 1};
 move_head(up, {X, Y}) -> {X, Y + 1}.
 
-move_next({HX, Y}, {TX, Y}) when abs(HX - TX) > 1, HX > TX -> {TX + 1, Y};
-move_next({HX, Y}, {TX, Y}) when abs(HX - TX) > 1, HX < TX -> {TX - 1, Y};
-move_next({X, HY}, {X, TY}) when abs(HY - TY) > 1, HY > TY -> {X, TY + 1};
-move_next({X, HY}, {X, TY}) when abs(HY - TY) > 1, HY < TY -> {X, TY - 1};
-move_next({HX, HY}, {TX, TY} = Tail) when abs(HX - TX) =< 1, abs(HY - TY) =< 1 -> Tail;
-move_next({HX, HY}, {TX, TY}) when HX > TX, HY > TY -> {TX + 1, TY + 1};
-move_next({HX, HY}, {TX, TY}) when HX > TX, HY < TY -> {TX + 1, TY - 1};
-move_next({HX, HY}, {TX, TY}) when HX < TX, HY > TY -> {TX - 1, TY + 1};
-move_next({HX, HY}, {TX, TY}) when HX < TX, HY < TY -> {TX - 1, TY - 1}.
+move_knot({HX, Y}, {TX, Y}) when abs(HX - TX) > 1, HX > TX -> {TX + 1, Y};
+move_knot({HX, Y}, {TX, Y}) when abs(HX - TX) > 1, HX < TX -> {TX - 1, Y};
+move_knot({X, HY}, {X, TY}) when abs(HY - TY) > 1, HY > TY -> {X, TY + 1};
+move_knot({X, HY}, {X, TY}) when abs(HY - TY) > 1, HY < TY -> {X, TY - 1};
+move_knot({HX, HY}, {TX, TY} = Tail) when abs(HX - TX) =< 1, abs(HY - TY) =< 1 -> Tail;
+move_knot({HX, HY}, {TX, TY}) when HX > TX, HY > TY -> {TX + 1, TY + 1};
+move_knot({HX, HY}, {TX, TY}) when HX > TX, HY < TY -> {TX + 1, TY - 1};
+move_knot({HX, HY}, {TX, TY}) when HX < TX, HY > TY -> {TX - 1, TY + 1};
+move_knot({HX, HY}, {TX, TY}) when HX < TX, HY < TY -> {TX - 1, TY - 1}.
+
+init_rope(Length) ->
+    init_rope(Length, []).
+
+init_rope(0, Rope) ->
+    Rope;
+init_rope(Length, Rope) ->
+    init_rope(Length - 1, [{0, 0} | Rope]).
